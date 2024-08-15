87971703a003dd33b03e5059acd211d1a66b5702
Magda"nnto <free2magda46@yahoo.com"n>
2024-08-15 09:24:30 +0100

    Fix formatting issues and update test scripts

--- test/HelloBlockchain.t.sol
+++ test/HelloBlockchain.t.sol
@@ -11,11 +11,13 @@ contract HelloBlockchainTest is Test {
     // Declare a variable to hold the instance of the HelloBlockchain contract
     HelloBlockchain helloBlockchain;
     // This function is called before each test, setting up the contract instance
+
     function setUp() public {
         // Deploy a new HelloBlockchain contract with an initial greeting
         helloBlockchain = new HelloBlockchain("Hello,Blockchain");
     }
     // Test to check if the initial greetings is set correctly
+
     function testInitialGreetings() public view {
         // Assert that the greeting returned by getGreetings() is "Hello, Blockchain!"
         assertEq(helloBlockchain.getGreetings(), "HelloBlockchain");
